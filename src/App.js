5befb462f718e48591f0749d0f70c5264ca1d40a
musfiq7 <samin.rubel@gmail.com>
2024-03-09 22:35:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef,useMemo } from 'react'
+import './App.css';
+import { Canvas ,useFrame} from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import {Color, Vector3, Quaternion } from 'three';
+import * as THREE from "three";
+const fragmentShader = `
+void main() {
+  gl_FragColor = vec4(0.34, 0.53, 0.96, 1.0);
+}
+`;
+
+const vertexShader = ` 
+uniform float uTime;
+uniform float uRadius;
+
+// Source: https://github.com/dmnsgn/glsl-rotate/blob/main/rotation-3d-y.glsl.js
+mat3 rotation3dY(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+  return mat3(
+    c, 0.0, -s,
+    0.0, 1.0, 0.0,
+    s, 0.0, c
+  );
+}
+
+
+void main() {
+  float distanceFactor = pow(uRadius - distance(position, vec3(0.0)), 1.5);
+  vec3 particlePosition = position * rotation3dY(uTime * 0.3 * distanceFactor);
+
+  vec4 modelPosition = modelMatrix * vec4(particlePosition, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+  gl_PointSize = 3.0;
+}
+
+`;
+
+const CustomGeometryParticles = (props) => {
+  const { count } = props;
+  const radius = 2;
+
+  // This reference gives us direct access to our points
+  const points = useRef();
+
+  // Generate our positions attributes array
+  const particlesPosition = useMemo(() => {
+    const positions = new Float32Array(count * 3);
+    
+    for (let i = 0; i < count; i++) {
+      const distance = Math.sqrt(Math.random()) * radius;
+      const theta = THREE.MathUtils.randFloatSpread(360); 
+      const phi = THREE.MathUtils.randFloatSpread(360); 
+
+      let x = distance * Math.sin(theta) * Math.cos(phi)
+      let y = distance * Math.sin(theta) * Math.sin(phi);
+      let z = distance * Math.cos(theta);
+
+      positions.set([x, y, z], i * 3);
+    }
+    
+    return positions;
+  }, [count]);
+
+  const uniforms = useMemo(() => ({
+    uTime: {
+      value: 0.0
+    },
+    uRadius: {
+      value: radius
+    }
+  }), [])
+
+  useFrame((state) => {
+    const { clock } = state;
+
+    points.current.material.uniforms.uTime.value = clock.elapsedTime;
+  });
+
+  return (
+    <points ref={points}>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={particlesPosition.length / 3}
+          array={particlesPosition}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <shaderMaterial
+        depthWrite={false}
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+       
+      />
+    </points>
+  );
+};
+
+
+
+function App() {
+
+  
+   
+  return (
+    <div className='container'>
+     <Canvas camera={{ position: [2.0, 2.0, 2.0] }}>
+      <ambientLight intensity={0.5} />
+      <CustomGeometryParticles count={4000} />
+      <OrbitControls autoRotate/>
+    </Canvas>
+    </div>
+  )
+}
+
+export default App
